8791620ae4a420035835892ff466dc63c1257b6b
root <root@bogon.(none)>
2013-08-21 03:47:37 -0700

    perl/output.log 	shell/file.sh

--- perl/logger.sh
+++ perl/logger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /data0/ChatServer11010/UCChatHall_log;
-for i in `ls -t|grep 455535`;do echo $i;cat $i|grep use_prop|grep 100000172;done
+cd /data0/ChatServer11020/UCChatHall_log;
+for i in `ls -t|grep 455135`;do echo $i;cat $i|grep use_prop|grep 100000172;done
