0fbafe130d231d1606b21497ea70746b02a218a7
yurnerola <yurnerola@gmail.com>
2013-08-30 04:18:51 -0700

    modified:   perl/logger.log 	modified:   perl/logger.sh

--- perl/logger.sh
+++ perl/logger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /data0/ChatServer11014/UCChatHall_log;
-for i in `ls -t|grep 1991`;do echo $i;cat $i|grep use_prop|grep 100000172;done
+cd /data0/ChatServer11113/UCChatHall_log;
+for i in `ls -t|grep 462719`;do echo $i;cat $i|grep use_prop|grep 100000172;done
