371e27cfb2e743a61883fbd015788415d5162d9d
yurnerola <yurnerola@gmail.com>
2013-09-03 04:58:35 -0700

    add some new files.learn python

--- nodejs/simpleserver.js
+++ nodejs/simpleserver.js
@@ -1,7 +1,9 @@
-#/usr/bin/env node
+#!/usr/bin/env node
 var http=require("http");
 http.createServer(function(request,response){
 	response.writeHead(200,{"Content-Type":"text/plain"});
 	response.write("HelloWorld");
+	console.log("---\n");
 	response.end();
-}).listen(8888);
+});
+//}).listen(8888);
