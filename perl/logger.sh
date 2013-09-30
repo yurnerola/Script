4311af1aaac933950f90991923d73e6eb562c2ca
yurnerola <yurnerola@gmail.com>
2013-09-29 19:01:08 -0700

    change vimrc

--- perl/logger.sh
+++ perl/logger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /data0/ChatServer/UCChatHall_log;
-for i in `ls -t|grep 475882`;do echo $i;cat $i|grep use_prop|grep 100000172;done
+cd /data0/ChatServer22167/UCChatHall_log;
+for i in `ls -t|grep 430892`;do echo $i;cat $i|grep use_prop|grep 100000172;done
